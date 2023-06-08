eea5aa645ca0fb144b34f4f6c3dd4a78f0a991bf
Elxem000 <135558430+Elxem000@users.noreply.github.com>
2023-06-08 23:47:41 +0300

    ElxUpdate.sh

--- ElxUpdate.sh
+++ ElxUpdate.sh
@@ -1,1 +1,5 @@
-git clone https://github.com/Elxem000/Elxbrut
+apt-get update
+apt-get upgrade
+pkg install myrepos
+rm -rf /data/data/com.termux/files/home/Elxbrut
+git klonu https://github.com/Elxem000/Elxbrut
