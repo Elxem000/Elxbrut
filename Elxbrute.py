4adb1b52da57d5ef5fde5ed579fb90e47cdc1e75
Elxem000 <135558430+Elxem000@users.noreply.github.com>
2023-06-09 13:38:15 +0300

    Elxbrute.py

--- Elxbrute.py
+++ Elxbrute.py
@@ -11,4 +11,22 @@ import subprocess as sub                              import random
 import sys
 
 
+clear
 
+print("EEEEEEEEEE") print("L")
+
+print("E")          print("L")
+
+print("E")          print("L")
+
+print("E")          print("L")
+
+print("EEEEEEE")    print("L")
+
+print("E")          print("L")
+
+print("E")          print("L")
+
+print("E")          print("L")
+
+print("EEEEEEEEEE") print("LLLLLLLLLLL")
