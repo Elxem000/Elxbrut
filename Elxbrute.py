0451cb1d23e148f06be245cce122853bc04cbff2
Elxem000 <135558430+Elxem000@users.noreply.github.com>
2023-06-10 16:03:14 +0300

    Elxbrute.py

--- Elxbrute.py
+++ Elxbrute.py
@@ -1,12 +1,18 @@
 
 from instabot import Bot
 
+import sleep
+
 print("ELXBRUT…⁠ᘛ⁠⁐̤⁠ᕐ⁠ᐷ")
+
 sleep(5)
 
-
 clear
+
 abe=input("kulanıcı adı")
+
 ahe=input("passlist girin")
+
 #login
+
 bot1.login(username="abe",password="ahe")
